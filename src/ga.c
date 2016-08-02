110fef6473a76c40f98ae93e2aae6094fd074073
Nick Stockton <sputnick1124@comcast.net>
2016-08-02 07:38:50 -0400

    fix random params bug, start on genetic operators

--- src/ga.c
+++ src/ga.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <time.h>
 #include "../include/ga.h"
 #include "../include/fuzzy.h"
 
+/** Some utility functions to take care of the mechanics **/
 int prod_i(int mults[], size_t len) {
 	int retval = 1;
 	int i;
@@ -22,6 +24,21 @@ int sum_i(int adds[], size_t len) {
 	return retval;
 }
 
+int rand_i(unsigned int max) {
+	/* return random integer in the range [0,max)*/
+	int r;
+	const unsigned int buckets = RAND_MAX / max;
+	const unsigned int limit = buckets * max;
+
+	do {
+		r = rand();
+	} while (r >= limit);
+
+	/* cast assumes that max is much less than RAND_MAX*/
+	return (int) (r / buckets);
+}
+
+/** Initialization functions for starting off the GA population**/
 void init_partition(double params[], int num_sets) {
 	int p;
 	int num_params = num_sets * 3;
@@ -43,6 +60,7 @@ void init_partition(double params[], int num_sets) {
 void rand_partition(double params[], int num_sets) {
 	int p;
 	double r;
+	double tmp;
 	int num_params = num_sets * 3;
 	srand48((long int)clock());
 	params[0] = 0; params[1] = 0; //Set lower bounds
@@ -52,10 +70,12 @@ void rand_partition(double params[], int num_sets) {
 
 	for (p = 2; p < num_params - 2; p++) {
 		r = drand48() * I; //random shift from partition division
+		printf("I = %0.2f\t r = %0.2f\n",I, r);
 		if (p == 3) {
 			params[p] = r; //Don't want the first element slipping below 0...
+			printf("p = r = %f\n",r);
 		} else if ( p == num_params - 4) {
-			params[p] = ((p / 3) - 1) * I - r; //...or the last past 1
+			params[p] = (((p - 2) / 3) + 1) * I - r; //...or the last past 1
 		} else if (p % 3 == 0) { //a_i
 			params[p] = ((p / 3) - 1) * I + r - (I / 2);
 		} else if (p % 3 == 1) { //b_i
@@ -98,7 +118,11 @@ void rand_params(double params[], int num_in, int in_mfs[], int num_out, int out
 	}
 }
 
-void init_antecedents(int num_in, int num_out, int rules[][num_in + num_out], int in_mfs[]) {
+void init_antecedents(int num_in,
+	int num_out,
+	int rules[][num_in + num_out],
+	int in_mfs[]) {
+
 	int in, rule;
 	unsigned int flag = 0;
 	int num_rule = 1;
@@ -129,3 +153,29 @@ void init_antecedents(int num_in, int num_out, int rules[][num_in + num_out], in
 		}
 	}
 }
+
+void rand_consequents(
+	int num_in,
+	int num_out,
+	int num_rule,
+	int rules[][num_in + num_out],
+	int out_mfs[]) {
+
+	int out, rule;
+	for (rule = 0; rule < num_rule; rule++) {
+		for (out = 0; out < num_out; out++) {
+			rules[rule][num_in + out] = rand_i(out_mfs[out]);
+		}
+	}
+}
+
+/** Crossover and mutation functions **/
+void
+sp_crossover(
+	int num_params,
+	double child1[],
+	double child2[],
+	double parent1[],
+	double parent2[])
+{
+/* Single-point crossover
--- src/test/ga_tests.c
+++ src/test/ga_tests.c
@@ -8,7 +8,7 @@ int main(int argc, char * argv[]) {
 	int num_out = 1;
 	int out_mfs[] = {3};
 	int num_params = 3 * (sum_i(in_mfs, num_in) + sum_i(out_mfs,num_out));
-	int num_rules = prod_i(in_mfs,num_in) * prod_i(out_mfs,num_out);
+	int num_rules = prod_i(in_mfs,num_in);
 	int rules[num_rules][num_in + num_out];
 	double params[num_params];
 	int rule, in, p;
@@ -19,6 +19,7 @@ int main(int argc, char * argv[]) {
 	rand_params(params, num_in, in_mfs, num_out, out_mfs);
 
 	init_antecedents(num_in, num_out, rules, in_mfs);
+	rand_consequents(num_in, num_out, num_rules, rules, out_mfs);
 
 	printf("[");
 	for (p = 0; p < num_params; p++) {
