4902f59e531f1b0f89d58367b1fd89c510411e74
Nick Stockton <sputnick1124@comcast.net>
2018-07-04 00:51:52 -0400

    move some things around. add simple makefile for main lib. sample makefile for single example. this project is really old and not very good quality. might start the whole thins ober in Rust. Will probably add a proper Python ctypes interface to this first, though and see how usable it is.

--- example/simple_fuzzy_example.c
+++ example/simple_fuzzy_example.c
@@ -4,77 +4,68 @@
 #include "fuzzy.h"
 
 
-int main(int argc, char * argv[]) {
-	int i;
-	double x[argc-1];
+double genfuzex(double x, double y) {
+//	struct Rule * rule_list[4];
 	double retval[1];
-	struct Rule * rule1;
-	struct Rule * rule2;
-	struct Rule * rule3;
-	struct Rule * rule4;
-	struct Rule * rule_list[4];
 
-	double size_small[3] = {0, 0, 1};
-	double size_large[3] = {0, 1, 1};
+	double params[] = {
+	0.0, 0.0, 0.2,
+	0.8, 1.0, 1.0,
+	0.0, 0.0, 0.5,
+	0.5, 1.0, 1.0,
+	0.0, 0.0, 0.5,
+	0.0, 0.5, 1.0,
+	0.5, 1.0, 1.0};
 
-	double weight_small[3] = {0, 0, 1};
-	double weight_large[3] = {0, 1, 1};
+/*	double params[] = {
+	0.0, 0.0, 1.0,
+	0.0, 1.0, 1.0,
+	0.0, 0.0, 1.0,
+	0.0, 1.0, 1.0,
+	0.0, 0.0, 0.5,
+	0.0, 0.5, 1.0,
+	0.5, 1.0, 1.0};*/
 
-	double qual_bad[3] = {0, 0, 0.5};
-	double qual_med[3] = {0, 0.5, 1};
-	double qual_good[3] = {0.5, 1, 1};
+	int rules[][3] = {
+	{0, 0, 0},
+	{0, 1, 1},
+	{1, 0, 1},
+	{1, 1, 2}};
 
-	double *input_list1[] = {size_small, weight_small};
-	double *input_list2[] = {size_small, weight_large};
-	double *input_list3[] = {size_large, weight_small};
-	double *input_list4[] = {size_large, weight_large};
+	int in_mfs[] = {2, 2};
+	int out_mfs[] = {3};
 
-	double *output_list1[] = {qual_bad};
-	double *output_list2[] = {qual_med};
-	double *output_list3[] = {qual_good};
+//	get_fis(rule_list, params, 2, 1, 4, rules, in_mfs, out_mfs);
+	struct Fis * fis = fis_create(params, 2,1,4, rules,in_mfs, out_mfs);
 
-	rule1 = create_rule(input_list1, 2, output_list1, 1);
-	rule2 = create_rule(input_list2, 2, output_list2, 1);
-	rule3 = create_rule(input_list3, 2, output_list2, 1);
-	rule4 = create_rule(input_list4, 2, output_list3, 1);
+	double xin[2];
+	xin[0] = x;
+	xin[1] = y;
+//	evalrules(retval, xin, fis->rule_list, fis->num_rule);
+	evalfis(retval, xin, fis);
 
-	rule_list[0] = rule1;
-	rule_list[1] = rule2;
-	rule_list[2] = rule3;
-	rule_list[3] = rule4;
+//	int r;
+//	for (r = 0; r < 4; r++) {
+//		destroy_rule(rule_list[r]);
+//	}
 
-	for (i = 1; i < argc; i++) {
-		x[i - 1] = atof(argv[i]);
+	fis_destroy(fis);
+	return retval[0];
+}
+
+
+int main(int argc, char *argv[]) {
+	double x, y, z;
+
+	if (argc < 2) {
+		printf("USAGE: %s <float> <float>\n",argv[0]);
+		exit(EXIT_FAILURE);
 	}
-	for (i = 1; i < argc; i++) {
-		printf("%0.2f\n",x[i - 1]);
-	}
+	x = atof(argv[1]);
+	y = atof(argv[2]);
 
-
-	evalrules(retval, x, rule_list, 4);
-
-	printf("%0.5f\n", retval[0]);
-	long long unsigned li;
-	for (li = 0; li < 100000; li++) {
-		x[0] += (1.0 + li) / li;
-		evalrules(retval, x, rule_list, 4);
-	}
-	for (li = 0; li < 100000; li++) {
-		x[0] += (1.0 + li) / li;
-		evalrules(retval, x, rule_list, 4);
-	}
-	for (li = 0; li < 100000; li++) {
-		x[0] += (1.0 + li) / li;
-		evalrules(retval, x, rule_list, 4);
-	}
-//	evalfis(retval, x, rule_list, 4);
-
-	int r;
-	for (r = 0; r < 4; r++) {
-		destroy_rule(rule_list[r]);
-	}
-
-
+	z = genfuzex(x,y);
+	printf("%f\n",z);
 	return 0;
 }
 
